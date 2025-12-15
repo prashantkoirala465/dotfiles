35efebcdf4e313e680efb955777dd05cbf88bc26
Prashant Koirala <92243577+prashantkoirala465@users.noreply.github.com>
2025-12-16 00:38:45 +0545

    feat: updated font for aerospace workspace

--- sketchybar/items/aerospace_workspace.sh
+++ sketchybar/items/aerospace_workspace.sh
@@ -4,7 +4,7 @@ sketchybar --add event aerospace_workspace_change
 
 sketchybar --add item workspace right \
             --set workspace script="$PLUGIN_DIR/aerospace_workspace.sh" \
-            label.font="SF Pro:Bold:17.0" \
+            label.font="SF Pro:Semibold:16.0" \
             label.padding_left=8 \
             label.padding_right=12 \
             icon.drawing=off \
