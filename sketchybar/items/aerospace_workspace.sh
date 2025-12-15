9bd64f1c38f2c0c742d6e2a3af82edd02bee6424
Prashant Koirala <92243577+prashantkoirala465@users.noreply.github.com>
2025-12-15 23:50:17 +0545

    feat: updated sketchybar & added aerospace space tracking

---
+++ sketchybar/items/aerospace_workspace.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sketchybar --add event aerospace_workspace_change
+
+sketchybar --add item workspace right \
+            --set workspace script="$PLUGIN_DIR/aerospace_workspace.sh" \
+            label.font="SF Pro:Bold:17.0" \
+            label.padding_left=8 \
+            label.padding_right=12 \
+            icon.drawing=off \
+            background.drawing=off \
+            update_freq=3 \
+            --subscribe workspace aerospace_workspace_change
