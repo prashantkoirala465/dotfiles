cfb0f788a2c9c8e4d07466214935710f60ec8418
Prashant Koirala <92243577+prashantkoirala465@users.noreply.github.com>
2025-12-16 00:30:10 +0545

    feat: updated music media not showing in sketchybar

--- sketchybar/items/media.sh
+++ sketchybar/items/media.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-sketchybar --add item media e \
+sketchybar --add item media center \
             --set media label.color=$ACCENT_COLOR \
             label.max_chars=20 \
+            label.scroll_duration=300 \
             icon.padding_left=0 \
             scroll_texts=on \
             icon=􀑪 \
             icon.color=$ACCENT_COLOR \
             background.drawing=off \
             script="$PLUGIN_DIR/media.sh" \
-            --subscribe media media_change
+            update_freq=5 \
+            --subscribe media media_change system_woke
